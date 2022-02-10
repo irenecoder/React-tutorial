723057898597c30c88fc86deb4cae8adb2dade3e
irenecoder <wairene60@gmail.com>
2022-02-10 12:32:36 +0300

    meme-image

--- src/components/meme/Meme.js
+++ src/components/meme/Meme.js
@@ -1,6 +1,6 @@
 import React from "react" 
 import "./meme.css"
-import memesData from "../App/memeData.js"
+import memesData from "./memesData.js"
 
 
 function Meme(){
@@ -18,8 +18,8 @@ function Meme(){
                     type="text"placeholder="top text" className="form-input"></input>
                 <input 
                     type="text"placeholder="bottom text" className="form-input"></input>
-                <button className="form-button">Get a new meme</button>
-                <img src={memeImage}></img>
+                <button className="form-button" onClick={getMemeImage}>Get a new meme</button>
+                <img src={memeImage} alt="meme-logo"className="meme-image"></img>
 
             </div>
 
---
+++ src/components/meme/memesData.js
@@ -0,0 +1,25 @@
+export default {
+    "success":true,
+    "data":{
+        "memes":[
+            {
+                "id":"181913649",
+                "name":"Drake Hotline Bling",
+                "url":"https://i.imgflip.com/30b1gx.jpg",
+                "width":1200,
+                "height":1200,
+                "box-count":2
+            },
+            {
+                "id":"112126428",
+                "name":"Distracted Boyfriend",
+                "url":"https://i.imgflip.com/1ur9b0.jpg",
+                "width":600,
+                "height":908, 
+                "box-count":4
+            },
+
+        ]
+    }
+}
+
