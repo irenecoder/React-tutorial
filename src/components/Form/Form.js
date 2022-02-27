af3bd159e8367de98ca3cfb60013727f972059ef
irenecoder <wairene60@gmail.com>
2022-02-27 17:51:50 +0300

    controlled inputs,checkbox

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,13 +2,15 @@ import React from "react"
 
 export default function Form(){
     const [formData,setFormData] = React.useState({
-        firstName:"",lastName:"",email:""
+        firstName:"",lastName:"",email:"",comments:"",isFriendly:true
     })
     console.log(formData)
     function handleChange(event){
+        const {name, value,type,checked} = event.target
         setFormData(prevFormData=>{
             return {
-                ...prevFormData,[event.target.name]:event.target.value,
+                ...prevFormData,
+                [name]:type ==="checkbox" ? checked:value
             }   
         }
         )
@@ -20,17 +22,38 @@ export default function Form(){
                 type="text"
                 placeholder="first name"
                 onChange ={handleChange}
-                name="firstName"></input>
+                name="firstName"
+                value = {formData.firstName}></input>
+                
                 <input 
                 type="text"
                 placeholder="last name"
                 onChange ={handleChange}
-                name="lastName"></input>
+                name="lastName"
+                value = {formData.lastName}></input>
+
                  <input 
                 type="email"
                 placeholder="email"
                 onChange ={handleChange}
-                name="email"></input>
+                name="email"
+                value = {formData.email}
+                ></input>
+
+                <textarea 
+                placeholder="textarea"
+                onChange ={handleChange}
+                name="comments"
+                value = {formData.comments}
+                />
+
+                <input 
+                type = "checkbox"
+                id="isFriendly"
+                checked={formData.isFriendly}
+                />
+                <br></br>
+                <label htmlFor="isFriendly">Are you friendly?</label>
             </form>
 
         )
